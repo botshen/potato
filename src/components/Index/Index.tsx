ed4d42d090acd4f208506921aeddeae6c70f496b
Xin shen <363088847@qq.com>
2021-09-16 21:04:01 +0800

    完成登出逻辑

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -1,27 +1,51 @@
 import * as React from 'react';
-import { Button } from "antd";
+import {Button} from 'antd';
+import axios from 'src/config/axios';
 
 interface IRouter {
-	history: any;
+    history: any;
 }
 
-class Component extends React.Component<IRouter> {
-
-	constructor(props: any){
-		super(props)
-	}
-
-	login = ()=>{
-		this.props.history.push('/login')
-	}
-
-	render() {
-		return (
-			<div className="Component">
-				<Button onClick={this.login}>登录</Button>
-			</div>
-		);
-	}
+interface IIndexState {
+    user: any;
 }
 
-export default Component;
+class Index extends React.Component<IRouter, IIndexState> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            user: {}
+        };
+
+    }
+
+    async componentWillMount() {
+        await this.getMe();
+    }
+
+    getMe = async () => {
+        try {
+            const response = await axios.get('me');
+            this.setState({user: response.data});
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    logout = () => {
+        localStorage.setItem('x-token', '');
+        this.props.history.push('/login');
+    };
+
+    render() {
+        return (
+            <div className="Component">
+                <p>欢迎，{this.state.user && this.state.user.account}</p>
+                <Button onClick={this.logout}>注销用户</Button>
+            </div>
+        );
+    }
+}
+
+export default Index;
