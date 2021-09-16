00655e93b250b0908e20c0d8c409ec29a4e6d2f6
Xin shen <363088847@qq.com>
2021-09-16 20:06:48 +0800

    完成登录页面

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import {Input, Icon, Button} from 'antd';
+import { Input, Icon, Button } from 'antd';
 import {Link} from 'react-router-dom';
-import axios from 'src/config/axios';
+import axios from 'src/config/axios'
 
-import './SignUp.scss';
+import './SignUp.scss'
 
 interface ISignUpState {
     account: string,
@@ -11,62 +11,56 @@ interface ISignUpState {
     passwordConformation: string
 }
 
-// 第一个是prop的类型，第二个是state的类型
-class SignUp extends React.Component<any, ISignUpState> {
-    constructor(props) {
-        super(props);
+class SignUp extends React.Component<any,ISignUpState> {
+    constructor(props){
+        super(props)
         this.state = {
             account: '',
             password: '',
             passwordConformation: ''
-        };
+        }
     }
 
     onChangeAccount = (e) => {
-        this.setState({account: e.target.value});
-    };
+        this.setState({ account: e.target.value });
+    }
 
     onChangePassword = (e) => {
-        this.setState({password: e.target.value});
-    };
+        this.setState({ password: e.target.value });
+    }
 
     onChangePasswordConformation = (e) => {
-        this.setState({passwordConformation: e.target.value});
-    };
+        this.setState({ passwordConformation: e.target.value });
+    }
 
     submit = async () => {
-        const {account, password, passwordConformation} = this.state;
-        try {
-            await axios.post('sign_up/user', {
+        const { account,password,passwordConformation } = this.state;
+        try{
+            await axios.post('sign_up/user',{
                 account, // account: account
                 password,
-                password_confirmation: passwordConformation
-            });
-            console.log('成功');
-        } catch (e) {
-            throw new Error(e);
+                password_confirmation:passwordConformation
+            })
+            this.props.history.push('/')
+        }catch(e){
+            throw new Error(e)
         }
-    };
-
-    linkTo = () => {
-        this.props.history.push('login');
-    };
+    }
 
     public render() {
-        const {account, password, passwordConformation} = this.state;
+        const { account,password,passwordConformation } = this.state;
         return (
             <div className="SignUp" id="SignUp">
-                <h1>番茄闹钟</h1>
+                <h1>番茄闹钟毕设注册</h1>
                 <Input
                     placeholder="请输入你的用户名"
-                    prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                     value={account}
                     onChange={this.onChangeAccount}
                 />
                 <Input.Password value={password} placeholder="请输入密码" onChange={this.onChangePassword}/>
-                <Input.Password value={passwordConformation} placeholder="请确认密码"
-                                onChange={this.onChangePasswordConformation}/>
-                <Button type="primary" className="loginButton" onClick={this.submit}>注册</Button>
+                <Input.Password value={passwordConformation} placeholder="请确认密码" onChange={this.onChangePasswordConformation}/>
+                <Button type="primary" className="signUpButton" onClick={this.submit}>注册</Button>
                 <p>如果你有账号，请立即 <Link to="/login">登录</Link></p>
             </div>
         );
