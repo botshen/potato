dbdacb42ef182c0019f96f2e4743cceaaaa856a2
Xin shen <363088847@qq.com>
2021-09-17 19:28:16 +0800

    1.优化 header 2.完成 TodoInput

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import {Button} from 'antd';
+import {Dropdown, Icon, Menu} from 'antd';
+import Todos from 'src/components/Todos/Todos';
 import axios from 'src/config/axios';
+import history from 'src/config/history';
+import './Index.scss';
 
 interface IRouter {
     history: any;
@@ -10,6 +13,18 @@ interface IIndexState {
     user: any;
 }
 
+const logout = () => {
+    localStorage.setItem('x-token', '');
+    history.push('/login');
+};
+
+const menu = (
+    <Menu>
+        <Menu.Item key="1"><Icon type="user"/>个人设置</Menu.Item>
+        <Menu.Item key="2" onClick={logout}><Icon type="logout"/>注销</Menu.Item>
+    </Menu>
+);
+
 class Index extends React.Component<IRouter, IIndexState> {
 
     constructor(props: any) {
@@ -17,7 +32,6 @@ class Index extends React.Component<IRouter, IIndexState> {
         this.state = {
             user: {}
         };
-
     }
 
     async componentWillMount() {
@@ -29,16 +43,21 @@ class Index extends React.Component<IRouter, IIndexState> {
         this.setState({user: response.data});
     };
 
-    logout = () => {
-        localStorage.setItem('x-token', '');
-        this.props.history.push('/login');
-    };
-
     render() {
         return (
-            <div className="Component">
-                <p>欢迎，{this.state.user && this.state.user.account}</p>
-                <Button onClick={this.logout}>注销用户</Button>
+            <div className="Index" id="Index">
+                <header>
+                    <span className="logo">LOGO</span>
+                    <Dropdown overlay={menu}>
+						<span>
+							{this.state.user && this.state.user.account}
+                            <Icon type="down" style={{marginLeft: 8}}/>
+						</span>
+                    </Dropdown>
+                </header>
+                <main>
+                    <Todos/>
+                </main>
             </div>
         );
     }
