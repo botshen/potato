10fc0948ea59ffc129b6a493d80ea08fe672674a
Xin shen <363088847@qq.com>
2021-10-05 16:02:10 +0800

    添加 todoItem 编辑功能

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,71 +1,82 @@
 import * as React from 'react';
-import TodoInput from 'src/components/Todos/TodoInput';
-import TodoItem from 'src/components/Todos/TodoItem';
-import axios from 'src/config/axios';
-import './Todos.scss';
+import TodoInput from 'src/components/Todos/TodoInput'
+import TodoItem from  'src/components/Todos/TodoItem'
+import axios from 'src/config/axios'
+import './Todos.scss'
 
 interface ITodosState {
     todos: any[];
 }
 
-class Todos extends React.Component<any, ITodosState> {
-    constructor(props) {
-        super(props);
+class Todos extends React.Component<any,ITodosState> {
+    constructor(props){
+        super(props)
         this.state = {
             todos: []
-        };
+        }
     }
 
-    addTodo = async (params: any) => {
-        const {todos} = this.state;
-        try {
-            const response = await axios.post('todos', params);
-            this.setState({todos: [response.data.resource, ...todos]});
-        } catch (e) {
-            throw new Error(e);
+    addTodo = async (params:any)=>{
+        const {todos} = this.state
+        try{
+            const response = await axios.post('todos',params)
+            this.setState({todos: [response.data.resource,...todos]})
+        }catch (e) {
+            throw new Error(e)
         }
-    };
+    }
 
-    componentDidMount() {
-        this.getTodos();
+    componentDidMount(){
+        this.getTodos()
     }
 
     getTodos = async () => {
-        try {
-            const response = await axios.get('todos');
-            this.setState({todos: response.data.resources});
-        } catch (e) {
-            throw new Error(e);
+        try{
+            const response = await axios.get('todos')
+            const todos = response.data.resources.map(t=>Object.assign({},t,{editing: false}))
+            this.setState({todos})
+        }catch (e) {
+            throw new Error(e)
         }
-    };
+    }
 
-    updateTodo = async (id: number, params: any) => {
-        const {todos} = this.state;
+    updateTodo = async (id:number,params:any) => {
+        const {todos} = this.state
         try {
-            const response = await axios.put(`todos/${id}`, params);
-            console.log('response');
-            console.log(response.data);
-            const newTodos = todos.map(t => {
-                if (id === t.id) {
-                    return response.data.resource;
+            const response = await axios.put(`todos/${id}`,params)
+            const newTodos = todos.map(t=>{
+                if (id === t.id){
+                    return response.data.resource
                 } else {
-                    return t;
+                    return t
                 }
-            });
-            this.setState({todos: newTodos});
-        } catch (e) {
-            throw new Error(e);
+            })
+            this.setState({todos: newTodos})
+        }catch (e) {
+            throw new Error(e)
         }
-    };
+    }
+
+    toEditing = (id:number) => {
+        const {todos} = this.state
+        const newTodos = todos.map(t=>{
+            if (id === t.id){
+                return Object.assign({},t,{editing: true})
+            } else {
+                return Object.assign({},t,{editing: false})
+            }
+        })
+        this.setState({todos: newTodos})
+    }
 
     public render() {
         return (
             <div className="Todos" id="Todos">
-                <TodoInput addTodo={(params) => this.addTodo(params)}/>
+                <TodoInput addTodo={(params)=>this.addTodo(params)}/>
                 <main>
                     {
-                        this.state.todos.map(t => <TodoItem key={t.id} {...t}
-                                                            update={this.updateTodo}
+                        this.state.todos.map(t=><TodoItem key={t.id} {...t}
+                                                          update={this.updateTodo} toEditing={this.toEditing}
                         />)
                     }
                 </main>
